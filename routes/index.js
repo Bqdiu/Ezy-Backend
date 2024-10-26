fb41a336ac0d929e61c48532c5d10ab6415b5392
hntung <hongoctungii@gmail.com>
2024-10-27 00:54:51 +0700

    event on categories

--- routes/index.js
+++ routes/index.js
@@ -108,7 +108,10 @@ const {
 const {
   getAllSaleEvents,
   addSaleEvent,
+  deleteSaleEvent,
+  addCategoriesToEvent,
 } = require("../controllers/SaleEventController");
+const { ro } = require("translate-google/languages");
 
 //------------------Categories-----------------------
 router.get("/categories", getAllCategories);
@@ -218,5 +221,6 @@ router.post("/checkout/ezywallet", checkoutWithEzyWallet);
 //-----------------SaleEvent-----------------
 router.get("/sale-events/get-event", getAllSaleEvents);
 router.post("/sale-events/add-event", addSaleEvent);
-
+router.delete("/sale-events/delete-event/:id", deleteSaleEvent);
+router.post("/sale-events/set-categories/:id", addCategoriesToEvent);
 module.exports = router;
